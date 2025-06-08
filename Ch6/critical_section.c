d10b82ab57b5817acf98f32cc8bae8e6fc497b95
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2025-06-08 21:58:54 +0900

    Create critical_section.c

---
+++ Ch6/critical_section.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUM_THREAD 10
+void *thread_inc(void *arg);
+void *thread_des(void *arg);
+long long num = 10;
+
+int main(int argc, char *argv[]){
+    pthread_t thread_id[NUM_THREAD];
+    int i;
+
+    printf("sizeof long long %ld \n", sizeof(long long));
+    for (i = 0 ; i < NUM_THREAD; i++){
+        if(i % 2){
+            pthread_create(&(thread_id[i]), NULL, thread_inc, NULL);
+        } else {
+            pthread_create(&(thread_id[i]), NULL, thread_des, NULL);
+        }
+    }
+
+    for (i = 0; i < NUM_THREAD ; i++)
+        pthread_join(thread_id[i],NULL);
+
+    printf("result: %lld \n" , num);
+    return 0;
+}
+
+void *thread_inc(void *arg){
+    int i;
+    for (i == 0 ; i < 10000; i++)
+        num +=1;
+
+    return NULL;
+}
+
+void *thread_des(void *arg){
+    int i;
+    for (i == 0 ; i < 10000; i++)
+        num -=1;
+
+    return NULL;
+}
